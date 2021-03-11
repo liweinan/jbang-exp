21fa36f61ff264d139b3c58f54c0fcb2a45f6202
阿男 <l.weinan@gmail.com>
2021-03-11 18:02:30 +0800

    try replace

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,9 +1,18 @@
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Foo {
-    public static void main(String[] args) {
-        Path path = Paths.get(".");
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get("foo.txt");
         System.out.println(path.toAbsolutePath());
+
+        Charset charset = StandardCharsets.UTF_8;
+        String content = new String(Files.readAllBytes(path), charset);
+        System.out.println(content);
+        System.out.println(content.replaceAll("_SPRING_VER", "1.0.0"));
+
     }
 }
